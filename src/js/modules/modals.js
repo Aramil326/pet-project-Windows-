3a76dc86f14787dfbae48bc8cafb1d5033ee614c
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-04-12 18:11:52 +0300

    Небольшие улучшения

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -3,7 +3,8 @@ const modals = () => {
     const trigger = document.querySelectorAll(triggerSelector),
       modal = document.querySelector(modalSelector),
       close = document.querySelector(closeSelector),
-      windows = document.querySelectorAll('[data-modal');
+      windows = document.querySelectorAll('[data-modal'),
+      scroll = calcScroll();
 
     trigger.forEach(item => {
       item.addEventListener('click', (e) => {
@@ -17,17 +18,18 @@ const modals = () => {
 
         modal.style.display = 'block';
         document.body.style.overflow = 'hidden';
-        // document.body.classlist.add('modal-open')
+        document.body.style.marginRight = `${scroll}px`;
       });
     });
 
     close.addEventListener('click', () => {
       windows.forEach(item => {
         item.style.display = 'none';
-      })
+      });
       modal.style.display = 'none';
       document.body.style.overflow = '';
-      // document.body.classlist.remove('modal-open')
+      document.body.style.marginRight = `0px`;
+
     });
 
     modal.addEventListener('click', (e) => {
@@ -37,7 +39,7 @@ const modals = () => {
         })
         modal.style.display = 'none';
         document.body.style.overflow = '';
-        // document.body.classlist.remove('modal-open')
+        document.body.style.marginRight = `0px`;
       }
     })
   }
@@ -47,6 +49,21 @@ const modals = () => {
       document.querySelector(selector).style.display = 'block'
       document.body.style.overflow = 'hidden';
     }, time)
+  }
+
+  function calcScroll() {
+    let div = document.createElement('div');
+
+    div.style.width = '50px';
+    div.style.height = '50px';
+    div.style.overflow = 'scroll';
+    div.style.visibility = 'hidden';
+
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+
+    return scrollWidth;
   }
 
   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
